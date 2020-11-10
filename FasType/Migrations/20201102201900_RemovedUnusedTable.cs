aaef71916add9f43238ab80624dabfa8396dd88e
Eyal Vayness <evayness@gmail.com>
2020-11-10 19:39:33 +0100

    Moved to .NET 5.0

---
+++ FasType/Migrations/20201102201900_RemovedUnusedTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FasType.Migrations
+{
+    public partial class RemovedUnusedTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
